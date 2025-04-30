5f4eb775ef5e1fa6e2d296d9c2eee0566b91b707
Hamid6426 <mianhamid6426@gmail.com>
2025-04-30 18:16:29 +0500

    spelling mistake

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 // src/App.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Homepage from "./pages/HomePage";
+import Homepage from "./pages/Homepage";
 import ContactForm from "./pages/ContactForm";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/contact" element={<ContactForm />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/contact" element={<ContactForm />} />
       </Routes>
     </BrowserRouter>
   );
